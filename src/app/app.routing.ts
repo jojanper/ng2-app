e9b4f12df483073e9e18d2c33728941c08ddb8bc
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-01-18 15:48:56 +0200

    Login and logout added to main menu.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,9 +5,11 @@ import { AboutComponent } from './about/about.component';
 import { DemoComponent } from './demo';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent},
-  { path: 'test', component: DemoComponent}
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'test', component: DemoComponent},
+
+  {path: '**', redirectTo: '/'}
 ];
 
 export const routing = RouterModule.forRoot(routes, {useHash: true});
