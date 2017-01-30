a1ea8f9430f3597b88e2883d77114fca056be0c3
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-01-30 15:34:55 +0200

    Unit test for AuthGuard.

---
+++ src/app/auth/auth.guard.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { CookieService } from 'angular2-cookie/core';
+
+import { AuthGuard } from './auth.guard';
+
+
+describe('AuthGuard', () => {
+
+    let user = null;
+    let mockCookie = {
+        getObject: () => {
+            return user;
+        }
+    };
+
+    let redirectUrl = null;
+    let mockRouter = {
+        navigate: (url) => {
+            redirectUrl = url[0];
+        }
+    };
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers:  [
+                AuthGuard,
+                {provide: CookieService, useValue: mockCookie},
+                {provide: Router, useValue: mockRouter},
+            ]
+        });
+    });
+
+    it('succeeds for authenticated user', inject([AuthGuard], (guard) => {
+        user = {username: 'test'};
+        expect(guard.canActivate(null, null)).toBeTruthy();
+        user = null;
+    }));
+
+    it('fails for unauthenticated user', inject([AuthGuard], (guard) => {
+        expect(guard.canActivate(null, {})).toBeFalsy();
+        expect(redirectUrl).toEqual('/login');
+    }));
+});
