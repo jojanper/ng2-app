99c8ae2521cdd53b8a0ad4ba6e79545b1079c257
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-03-20 22:55:30 +0200

    Remove forms example from test page (1st tab).

--- src/app/pages/form/index.ts
+++ src/app/pages/form/index.ts
@@ -1,2 +1,1 @@
-export * from './app-form.component';
 export * from './demo-form.component'
--- src/app/pages/form/model.ts
+++
@@ -1,8 +0,0 @@
-export class AppFormModel {
-  constructor (
-    public id: number,
-    public name: string,
-    public power: string,
-    public alterEgo?: string
-  ) {  }
-}
