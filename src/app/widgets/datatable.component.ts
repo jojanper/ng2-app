433f7991793b11adaf7d574eb046ebf8abfdced2
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-03-21 15:45:02 +0200

    Enable Bootstrap 4 support by using local copy of relevant DataTables file. (#10)
    Remove DataTables typings file, not needed.

--- src/app/widgets/datatable.component.ts
+++ src/app/widgets/datatable.component.ts
@@ -1,7 +1,19 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import * as $ from 'jquery';
+import 'jquery';
 import 'datatables.net';
-import 'datatables.net-bs';
+declare var $: any;
+
+/**
+ * Until Bootstrap 4 is officially supported by DataTables, use local copy of the bootstrap plugin.
+ *
+ * https://datatables.net/examples/styling/bootstrap4.html
+ *
+ * NOTE: The datatables.net-bs/js/dataTables.bootstrap.js need to be modified also to work with bootstrap alpha.6;
+ *     see https://github.com/DataTables/DataTablesSrc/commit/dbc18cabe53bc20369e24e30d2d1c151f68b6190,
+ *     use https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap4.js as basis for modifications.
+ */
+// import 'datatables.net-bs';
+import './datatable.bootstrap4';
 
 @Component({
     selector: 'dng2-dt',
@@ -9,8 +21,23 @@ import 'datatables.net-bs';
 })
 export class WidgetDtComponent implements AfterViewInit {
     @ViewChild('dtElem') el: ElementRef;
+    @ViewChild('dtElem2') el2: ElementRef;
 
     ngAfterViewInit() {
         $(this.el.nativeElement).DataTable();
+
+        const options: any = {
+            columns: [
+                {
+                    title: 'Test',
+                    data: 'name'
+                }
+            ],
+            data: [
+                {name: 'A'},
+                {name: 'B'}
+            ]
+        };
+        $(this.el2.nativeElement).DataTable(options);
     }
 }
