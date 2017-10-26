329b1f4b09f5ace4a5dd34a610c264b2834137ea
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-10-26 23:18:24 +0300

    Auth pages as own module.

---
+++ src/app/pages/auth/auth.module.ts
@@ -0,0 +1,6 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppEmptyViewComponent } from '../../widgets';
+import { AuthGuard, LoginComponent, LogoutComponent, RegisterComponent } from '../../widgets/auth';
+
