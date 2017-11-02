0f84d1f76d01011f406241fc7a0a2af81472bfc9
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-11-02 09:04:33 +0200

    More clean up.

--- src/app/widgets/auth/auth.module.ts
+++ src/app/widgets/auth/auth.module.ts
@@ -3,14 +3,9 @@ import { CommonModule } from '@angular/common';
 import { CookieService } from 'angular2-cookie/services/cookies.service';
 
 import { AuthGuard } from './auth.guard';
-// import { LoginComponent, LogoutComponent } from './login';
-// import { RegisterComponent } from './registration';
-// import { DraalFormsModule } from '../form/form.module';
 
 @NgModule({
-  imports: [CommonModule/*, DraalFormsModule.forRoot()*/],
-  // declarations: [LoginComponent, LogoutComponent, RegisterComponent],
-  // exports: [LoginComponent, LogoutComponent, RegisterComponent]
+  imports: [CommonModule]
 })
 export class DraalAuthModule {
   static forRoot(): ModuleWithProviders {
