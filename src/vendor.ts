e9149e67f67581e85e98e50a8f9277b6f2ebbfdf
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-11-16 15:49:08 +0200

    Switch to Angular 5.

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,7 +1,7 @@
-// Angular 2
+// Angular
+import '@angular/core';
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
-import '@angular/core';
 import '@angular/common';
 import '@angular/router';
 
