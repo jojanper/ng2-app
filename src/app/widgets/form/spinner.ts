3dd1144bb3b4df6c702f37a5b50fc3eb0feb6141
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-12-12 15:38:39 +0200

    Finalize StateTrackerObservable.

--- src/app/widgets/form/spinner.ts
+++
@@ -1,9 +0,0 @@
-import { AppObserver } from '../../widgets/base';
-
-
-class FormTracker extends AppObserver<ProgressTracker> {
-
-    setState(state: string): void {
-        this.setSubject({id: id, type: this.type, text: text});
-    }
-}
