d8c2be427952cc051453983c3fb1937520cde6bb
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-12-14 15:38:40 +0200

    Rename planets for starwars.

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -2,6 +2,6 @@ export * from './about/about.component';
 export * from './home/home.component';
 export * from './header';
 export * from './footer';
-export * from './planets';
+export * from './starwars';
 export { DraalAppRoutes } from './pages.routing';
 export { DraalAppPagesModule } from './pages.module';
