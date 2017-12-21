e742ee49c024219d1e4fbd211fbbf7e4eb54ec99
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-12-21 21:26:04 +0200

    Clean unit tests module.

--- src/app/pages/demo/demo.component.spec.ts
+++ src/app/pages/demo/demo.component.spec.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
-import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 import { APP_BASE_HREF } from '@angular/common';
 
 import { AppFormComponent } from './form';
@@ -14,132 +14,137 @@ import { DraalDataTableModule, DraalAlertModule, DraalFormsModule,
 import { TestServiceHelper } from '../../../test_helpers';
 
 
-describe('Demo Component', () => {
-  let fixture: ComponentFixture<DemoComponent>;
-
-  function clickTab(tabIndex) {
-    const tab = fixture.nativeElement.querySelectorAll('li.nav-item a')[tabIndex];
-    tab.click();
-    fixture.detectChanges();
-  }
-
-  const mockAlert = new TestServiceHelper.alertService();
-
-  beforeEach(async(() => {
-    // https://github.com/angular/angular/issues/10760
-    TestBed.configureTestingModule({
+@NgModule({
     imports: [
-      CommonModule,
-      RouterModule.forRoot([{
-        path: '', component: DemoComponent
-      }]),
-      NgbModule.forRoot(),
-      DraalFormsModule,
-      DraalDataTableModule,
-      DraalAlertModule,
-      DraalServicesModule,
-      DraalWidgetsCoreModule
-    ],
-    providers: [
-      {provide: AlertService, useValue: mockAlert},
-      {provide: APP_BASE_HREF, useValue: '/'}
+        CommonModule,
+        RouterModule.forRoot([{
+            path: '', component: DemoComponent
+        }]),
+        NgbModule.forRoot(),
+        DraalFormsModule,
+        DraalDataTableModule,
+        DraalAlertModule,
+        DraalServicesModule,
+        DraalWidgetsCoreModule
     ],
     declarations: [
-      AppDialogComponent,
-      AppFormComponent,
-      DemoComponent,
-      RouteComponent
-    ],
-    }).overrideModule(BrowserDynamicTestingModule, {
-      set: {
-        entryComponents: [RouteComponent]
-      }
-    }).compileComponents().then(() => {
-      fixture = TestBed.createComponent(DemoComponent);
-    });
-  }));
+        AppDialogComponent,
+        AppFormComponent,
+        DemoComponent,
+        RouteComponent
+      ],
+    entryComponents: [
+        RouteComponent
+    ]
+})
+class DemoModule {}
 
-  it('should have 5 tabs', async(() => {
-    // GIVEN demo page
-    // WHEN building demo component
-    fixture.detectChanges();
 
-    fixture.whenStable().then(() => {
-      // THEN page should have title
-      expect(fixture.nativeElement.querySelector('h2').textContent).toContain('Collection of UI components');
+describe('Demo Component', () => {
+    let fixture: ComponentFixture<DemoComponent>;
 
-      // AND 5 tabs
-      expect(fixture.nativeElement.querySelectorAll('li.nav-item').length).toEqual(5);
-    });
-  }));
+    function clickTab(tabIndex) {
+        const tab = fixture.nativeElement.querySelectorAll('li.nav-item a')[tabIndex];
+        tab.click();
+        fixture.detectChanges();
+    }
 
-  it('1st tab is selected', async(() => {
-    // GIVEN demo page
-    fixture.detectChanges();
+    const mockAlert = new TestServiceHelper.alertService();
 
-    // WHEN clicking first tab
-    clickTab(0);
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [DemoModule],
+            providers: [
+                {provide: AlertService, useValue: mockAlert},
+                {provide: APP_BASE_HREF, useValue: '/'}
+            ]
+        }).compileComponents().then(() => {
+            fixture = TestBed.createComponent(DemoComponent);
+        });
+    }));
 
-    // THEN dialog button should be visible
-    fixture.whenStable().then(() => {
-        expect(fixture.nativeElement.querySelectorAll('dng-app-dialog').length).toEqual(1);
-    });
-  }));
+    it('should have 5 tabs', async(() => {
+        // GIVEN demo page
+        // WHEN building demo component
+        fixture.detectChanges();
 
-  it('2nd tab is selected', async(() => {
-    // GIVEN demo page
-    fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            // THEN page should have title
+            expect(fixture.nativeElement.querySelector('h2').textContent)
+                .toContain('Collection of UI components');
 
-    // WHEN clicking second tab
-    clickTab(1);
+          // AND 5 tabs
+          expect(fixture.nativeElement.querySelectorAll('li.nav-item').length).toEqual(5);
+        });
+    }));
 
-    // THEN chosen plugin should be visible
-    fixture.whenStable().then(() => {
-        expect(fixture.nativeElement.querySelectorAll('dng-app-form').length).toEqual(1);
-    });
-  }));
+    it('1st tab is selected', async(() => {
+        // GIVEN demo page
+        fixture.detectChanges();
 
-  it('3rd tab is selected', async(() => {
-    // GIVEN demo page
-    fixture.detectChanges();
+        // WHEN clicking first tab
+        clickTab(0);
 
-    // WHEN clicking third tab
-    clickTab(2);
+        // THEN dialog button should be visible
+        fixture.whenStable().then(() => {
+            expect(fixture.nativeElement.querySelectorAll('dng-app-dialog').length).toEqual(1);
+        });
+    }));
 
-    // THEN datatables plugin(s) should be visible
-    fixture.whenStable().then(() => {
-        expect(fixture.nativeElement.querySelectorAll('dng-dt').length).toEqual(3);
-    });
-  }));
+    it('2nd tab is selected', async(() => {
+        // GIVEN demo page
+        fixture.detectChanges();
 
-  it('4th tab is selected', async(() => {
-    // GIVEN demo page
-    fixture.detectChanges();
+        // WHEN clicking second tab
+        clickTab(1);
 
-    // WHEN clicking 4th tab
-    clickTab(3);
+        // THEN chosen plugin should be visible
+        fixture.whenStable().then(() => {
+            expect(fixture.nativeElement.querySelectorAll('dng-app-form').length).toEqual(1);
+        });
+    }));
 
-    // THEN alert buttons should be available
-    fixture.whenStable().then(() => {
-        const buttons = fixture.nativeElement.querySelectorAll('button');
-        expect(buttons.length).toEqual(4);
+    it('3rd tab is selected', async(() => {
+        // GIVEN demo page
+        fixture.detectChanges();
 
-        // AND alerts messages can be created by clicking the available buttons
-        createAlert(buttons[0], 'success');
-        createAlert(buttons[1], 'info');
-        createAlert(buttons[2], 'warning');
-        createAlert(buttons[3], 'error');
-    });
-  }));
+        // WHEN clicking third tab
+        clickTab(2);
 
-  function createAlert(button, alertType) {
-      // WHEN creating alert messages
-      button.click();
-      fixture.detectChanges();
+        // THEN datatables plugin(s) should be visible
+        fixture.whenStable().then(() => {
+            expect(fixture.nativeElement.querySelectorAll('dng-dt').length).toEqual(3);
+        });
+    }));
 
-      // THEN call to alert service is made
-      fixture.whenStable().then(() => {
-        expect(mockAlert.getCallsCount(alertType)).toEqual(1);
-      });
+    it('4th tab is selected', async(() => {
+        // GIVEN demo page
+        fixture.detectChanges();
+
+        // WHEN clicking 4th tab
+        clickTab(3);
+
+        // THEN alert buttons should be available
+        fixture.whenStable().then(() => {
+            const buttons = fixture.nativeElement.querySelectorAll('button');
+            expect(buttons.length).toEqual(4);
+
+            // AND alerts messages can be created by clicking the available buttons
+            createAlert(buttons[0], 'success');
+            createAlert(buttons[1], 'info');
+            createAlert(buttons[2], 'warning');
+            createAlert(buttons[3], 'error');
+        });
+    }));
+
+    function createAlert(button, alertType) {
+        // WHEN creating alert messages
+        button.click();
+        fixture.detectChanges();
+
+        // THEN call to alert service is made
+        fixture.whenStable().then(() => {
+            expect(mockAlert.getCallsCount(alertType)).toEqual(1);
+        });
     }
 });
