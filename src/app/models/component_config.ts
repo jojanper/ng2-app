71d07b9e05d83766720f877df4643a688d613aed
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-01-22 22:45:31 +0200

    Codestyle.

--- src/app/models/component_config.ts
+++ src/app/models/component_config.ts
@@ -1,7 +1,7 @@
 export interface FormConfigData {
     type: string;
     label: string;
-    noLabel?: boolean,
+    noLabel?: boolean;
     placeholder: string;
     validators: Array<any>;
     groupvalidators?: Array<any>;
