45ace96630d74c5b38c2739879116d9a7b1d27e7
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-02-18 22:59:30 +0200

    Fix unit tests.

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -3,7 +3,6 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 import { RouteManager } from '../../router';
-import { DropdownItem } from '../../widgets';
 import { State } from '../../application/app.reducers';
 import { getUserAuthenticationStatus } from '../../rx/rx.reducers';
 
@@ -18,31 +17,6 @@ export class DraalAppHeaderComponent {
 
     // Menu items on the left-hand side of the header component
     menuLeft = RouteManager.topMenuItems('left');
-
-    // Menu items on the right-hand side of the header component
-    menuRight = RouteManager.topMenuItems('right');
-
-    authMenuItems = [
-        DropdownItem.createAsRoute({
-            url: RouteManager.resolveByName('login-view'),
-            title: 'Sign in'
-        }),
-        DropdownItem.createAsLink({
-            url: 'http://yle.fi',
-            title: 'Yle'
-        }),
-        DropdownItem.createAsDivider(),
-        DropdownItem.createAsLink({
-            url: 'http://nokia.fi',
-            title: 'Nokia'
-        }),
-        DropdownItem.createAsCallback({
-            url: '',
-            title: 'Nokia'
-        }, () => {
-            console.log(this);
-        })
-    ];
 
     constructor(protected store: Store<State>) {
         this.authStatus = this.store.select(getUserAuthenticationStatus);
