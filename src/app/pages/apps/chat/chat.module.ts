28254a5c33a894a05b0534dadfaa2acd2aa61d87
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-06-01 22:11:15 +0300

    Move chat routes

--- src/app/pages/apps/chat/chat.module.ts
+++ src/app/pages/apps/chat/chat.module.ts
@@ -1,12 +1,45 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Route } from '@angular/router';
+
+import { RouteManager } from '../../../router';
+import { AppEmptyViewComponent } from '../../../utils/base';
+import { DraalWidgetsCoreModule, DraalFormsModule } from '../../../widgets';
 
 import { ChatComponent } from './chat.component';
 import { VideoChatComponent } from './videochat.component';
 import { SocketService } from './services';
-import { DraalWidgetsCoreModule, DraalFormsModule } from '../../../widgets';
+import { CHATROUTES } from './chat.routes.config';
 
-export * from './chat.routing';
+
+const getChatRoutes = (config) => {
+    return [
+        {
+            path: config['users'].url,
+            component: ChatComponent,
+            data: {
+                config: RouteManager.getConfig(config['users'].name)
+            }
+        },
+        {
+            path: config['video'].url,
+            component: VideoChatComponent,
+            data: {
+                config: RouteManager.getConfig(config['video'].name)
+            }
+        }
+    ];
+};
+
+export const CHATROUTE: Route = {
+    path: CHATROUTES.url,
+    component: AppEmptyViewComponent,
+    data: {
+        config: RouteManager.getConfig(CHATROUTES.name)
+    },
+    children: getChatRoutes(CHATROUTES.children)
+};
+
 
 @NgModule({
     imports: [
