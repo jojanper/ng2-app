a27b5e30b8c80061991df85ce300ff5ab56cb38a
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-11-19 07:31:44 +0200

    Update tests.

--- src/app/services/auth/autologout.spec.ts
+++ src/app/services/auth/autologout.spec.ts
@@ -10,9 +10,11 @@ import { reducers } from '../../rx/rx.reducers';
 
 const USER = AuthResponseFixture.User();
 
+
 describe('AutoLogout Service', () => {
     let service: AutoLogout;
     let store: any;
+    let s: any;
 
     const mockAlert = new TestServiceHelper.alertService();
 
@@ -33,6 +35,8 @@ describe('AutoLogout Service', () => {
             service = getTestBed().get(AutoLogout);
             store = getTestBed().get(Store);
             spyOn(store, 'dispatch').and.callThrough();
+            s = service;
+            spyOn(s.loginSubscription, 'unsubscribe').and.callThrough();
             done();
         });
     });
@@ -41,6 +45,7 @@ describe('AutoLogout Service', () => {
         service.ngOnDestroy();
     });
 
+    /*
     function logoutTest(offset = 0) {
         // WHEN user state changes to authenticated
         const authAction = new AuthActions.LoginSuccessAction(USER);
@@ -59,9 +64,25 @@ describe('AutoLogout Service', () => {
         const action = store.dispatch.calls.argsFor(offset + 1)[0];
         expect(action.type).toEqual(AuthActions.ActionTypes.LOGOUT);
     }
+    */
 
     it('user session expires', fakeAsync(() => {
-        logoutTest();
+        // WHEN user state changes to authenticated
+        const authAction = new AuthActions.LoginSuccessAction(USER);
+        store.dispatch(authAction);
+        discardPeriodicTasks();
+
+        // AND time passes the session expiration time
+        tick(3000);
+
+        // THEN info regarding session expiration is shared to user
+        expect(mockAlert.getCallsCount('info')).toEqual(1);
+
+        // AND logout action is fired
+        const ncalls = store.dispatch.calls.count();
+        expect(ncalls).toEqual(2);
+        const action = store.dispatch.calls.argsFor(1)[0];
+        expect(action.type).toEqual(AuthActions.ActionTypes.LOGOUT);
     }));
 
     fit('user logouts before session expiration', fakeAsync(() => {
@@ -72,18 +93,27 @@ describe('AutoLogout Service', () => {
         //const s1 = service.loginSubscription;
         //let subscription: any;
         //subscription = service.loginSubscription;
-        let s: any;
-        s = service;
 
-        spyOn(s.loginSubscription, 'unsubscribe').and.callThrough();
-        const authAction = new AuthActions.LogoutAction();
+        const s1 = s.loginSubscription;
+        expect(s1.unsubscribe.calls).toBeDefined();
+
+        const authAction2 = new AuthActions.LoginSuccessAction(USER);
+        store.dispatch(authAction2);
+
+        //console.log('START');
+
+        const authAction = new AuthActions.LogoutSuccessAction('auth.login-view')
         store.dispatch(authAction);
         //const s2 = service.loginSubscription;
 
         //expect(s1).toEqual(s2);
 
-        logoutTest(1);
+        //logoutTest(2);
 
-        console.log(s.loginSubscription.unsubscribe.calls.count());
+        //expect(s1.loginSubscription.unsubscribe.calls).toBeDefined();
+        expect(s.loginSubscription.unsubscribe.calls).toBeUndefined();
+        //expect(s1).toEqual(s.loginSubscription);
+
+        //console.log(s.loginSubscription.unsubscribe.calls.count());
     }));
 });
