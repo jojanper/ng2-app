97ea0264ca8c46fa2378e889ac349fee9dffb81f
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-11-19 22:11:10 +0200

    Header store select fixed

--- src/app/application/app.component.spec.ts
+++ src/app/application/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { provideRoutes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Store } from '@ngrx/store';
+import { Store, StoreModule, combineReducers } from '@ngrx/store';
 
 import { DraalServicesModule, ApiService, AppEventsService,
     AppEventTypes, AutoLogout } from '../services';
@@ -9,16 +9,19 @@ import { AppComponent } from './app.component';
 import { DraalAlertModule, SideMenuComponent, BreadcrumbComponent,
     DropDownComponent, UserMenuComponent } from '../widgets';
 import { DraalAppHeaderComponent, DraalAppFooterComponent } from '../pages';
-import { TestServiceHelper, TestObservablesHelper } from '../../test_helpers';
 import * as AuthActions from '../rx/auth';
+import { reducers } from '../rx/rx.reducers';
 
 
-const testModuleDef = (events: any, mockApi: any, mockStore: any, mockLogout: any) => {
+const testModuleDef = (events: any, mockApi: any, mockLogout: any) => {
     return {
         imports: [
             RouterTestingModule,
             DraalAlertModule.forRoot(),
-            DraalServicesModule.forRoot()
+            DraalServicesModule.forRoot(),
+            StoreModule.forRoot({
+                'apprx': combineReducers(reducers)
+            })
         ],
         declarations: [
             DraalAppHeaderComponent, DraalAppFooterComponent, AppComponent,
@@ -27,7 +30,6 @@ const testModuleDef = (events: any, mockApi: any, mockStore: any, mockLogout: an
         ],
         providers: [
             provideRoutes([]),
-            {provide: Store, useValue: mockStore},
             {provide: AppEventsService, useValue: events},
             {provide: ApiService, useValue: mockApi},
             {provide: AutoLogout, useValue: mockLogout}
@@ -36,18 +38,19 @@ const testModuleDef = (events: any, mockApi: any, mockStore: any, mockLogout: an
 };
 
 describe('App Component', () => {
-
     const mockApi = {};
     let fixture: ComponentFixture<AppComponent>;
+    let store: any;
 
     const events = new AppEventsService();
-    const authStatus = new TestObservablesHelper.getUserAuthenticationStatus();
-    const mockStore = new TestServiceHelper.store([authStatus.observable]);
     const mockLogout = {};
 
     beforeEach(done => {
-        const ref = testModuleDef(events, mockApi, mockStore, mockLogout);
+        const ref = testModuleDef(events, mockApi, mockLogout);
         TestBed.configureTestingModule(ref).compileComponents().then(() => {
+            store = TestBed.get(Store);
+            spyOn(store, 'dispatch').and.callThrough();
+
             fixture = TestBed.createComponent(AppComponent);
             fixture.detectChanges();
             done();
@@ -55,7 +58,7 @@ describe('App Component', () => {
     });
 
     it('user cookie is loaded on start up', () => {
-        const action = <AuthActions.UserCookieLoadAction>mockStore.getDispatchAction(0);
+        const action = store.dispatch.calls.argsFor(0)[0];
         expect(action.type).toEqual(AuthActions.ActionTypes.LOAD_AUTH_COOKIE);
     });
 
