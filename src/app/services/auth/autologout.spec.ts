9beb4ef99252a896dd886666b1243faf24daf9c9
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-11-19 21:38:08 +0200

    Fix test.

--- src/app/services/auth/autologout.spec.ts
+++ src/app/services/auth/autologout.spec.ts
@@ -45,27 +45,6 @@ describe('AutoLogout Service', () => {
         service.ngOnDestroy();
     });
 
-    /*
-    function logoutTest(offset = 0) {
-        // WHEN user state changes to authenticated
-        const authAction = new AuthActions.LoginSuccessAction(USER);
-        store.dispatch(authAction);
-        discardPeriodicTasks();
-
-        // AND time passes the session expiration time
-        tick(3000);
-
-        // THEN info regarding session expiration is shared to user
-        expect(mockAlert.getCallsCount('info')).toEqual(1);
-
-        // AND logout action is fired
-        const ncalls = store.dispatch.calls.count();
-        expect(ncalls).toEqual(offset + 2);
-        const action = store.dispatch.calls.argsFor(offset + 1)[0];
-        expect(action.type).toEqual(AuthActions.ActionTypes.LOGOUT);
-    }
-    */
-
     it('user session expires', fakeAsync(() => {
         // WHEN user state changes to authenticated
         const authAction = new AuthActions.LoginSuccessAction(USER);
@@ -85,35 +64,22 @@ describe('AutoLogout Service', () => {
         expect(action.type).toEqual(AuthActions.ActionTypes.LOGOUT);
     }));
 
-    fit('user logouts before session expiration', fakeAsync(() => {
-        // When user state is unauthenticated, no actions are fired
-        //userState.setAuthStatus(false);
-        //const action = <LogoutAction>mockStore.getDispatchAction(0);
-        //expect(action).toEqual(undefined);
-        //const s1 = service.loginSubscription;
-        //let subscription: any;
-        //subscription = service.loginSubscription;
-
+    it('user logouts before session expiration', fakeAsync(() => {
+        // When logout occurs for authenticated user, the existing
+        // login subscription is terminated and new subscription is
+        // started to wait for user login.
+        // -> Test that login subscription is reset
+        //    -> Reset will override the current Jasmine spy
         const s1 = s.loginSubscription;
-        expect(s1.unsubscribe.calls).toBeDefined();
 
         const authAction2 = new AuthActions.LoginSuccessAction(USER);
         store.dispatch(authAction2);
 
-        //console.log('START');
+        expect(s1.unsubscribe.calls).toBeDefined();
 
         const authAction = new AuthActions.LogoutSuccessAction('auth.login-view')
         store.dispatch(authAction);
-        //const s2 = service.loginSubscription;
 
-        //expect(s1).toEqual(s2);
-
-        //logoutTest(2);
-
-        //expect(s1.loginSubscription.unsubscribe.calls).toBeDefined();
         expect(s.loginSubscription.unsubscribe.calls).toBeUndefined();
-        //expect(s1).toEqual(s.loginSubscription);
-
-        //console.log(s.loginSubscription.unsubscribe.calls.count());
     }));
 });
