784a6aa5de9c0d58ac2ea1358160b1f04e89345c
Juha Ojanperä <juha.ojanpera@gmail.com>
2019-01-21 22:31:17 +0200

    Initial test updates, clean scroll directive code.

--- src/app/pages/api/brewery/components/beers/beers.component.spec.ts
+++ src/app/pages/api/brewery/components/beers/beers.component.spec.ts
@@ -1,15 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpTestingController } from '@angular/common/http/testing';
 
 import { BeersComponent } from './beers.component';
+import { DraalWidgetsCoreModule } from '../../../../../widgets';
+import { NetworkService, AlertService } from '../../../../../services';
+import { TestHttpHelper, TestServiceHelper } from '../../../../../../test_helpers';
 
 
 describe('BeersComponent', () => {
     let component: BeersComponent;
     let fixture: ComponentFixture<BeersComponent>;
+    let mockBackend: HttpTestingController;
+
+    const mockAlert = new TestServiceHelper.alertService();
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [ BeersComponent ]
+            imports: [
+                CommonModule,
+                DraalWidgetsCoreModule,
+            ].concat(TestHttpHelper.http),
+            declarations: [
+                BeersComponent
+            ],
+            providers: [
+                NetworkService,
+                {provide: AlertService, useValue: mockAlert},
+            ]
         })
         .compileComponents();
     }));
@@ -18,9 +36,10 @@ describe('BeersComponent', () => {
         fixture = TestBed.createComponent(BeersComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+        mockBackend = TestHttpHelper.getMockBackend();
     });
 
-    it('should create', () => {
+    fit('should create', () => {
         expect(component).toBeTruthy();
     });
 });
--- src/app/widgets/directives/scroll.directive.ts
+++ src/app/widgets/directives/scroll.directive.ts
@@ -1,16 +1,9 @@
 import { Directive, Input, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { fromEvent } from 'rxjs';
-import { tap, pairwise, map, filter, debounceTime, distinct, startWith } from 'rxjs/operators';
+import { map, filter, debounceTime, distinct } from 'rxjs/operators';
 
 import { AppBaseTrackerComponent } from '../../utils/base';
 
-
-const DEFAULT_SCROLL_POSITION = {
-    wY: 0,
-    cH: 0
-}
-
-// https://blog.strongbrew.io/infinite-scroll-with-rxjs-and-angular2/
 
 @Directive({
     selector: '[dngScroller]',
@@ -25,77 +18,43 @@ export class ScrollerDirective extends AppBaseTrackerComponent implements AfterV
     }
 
     ngAfterViewInit() {
-        //console.log('HEP');
         let prevHeight = 0;
-        fromEvent(window/*this.elementRef.nativeElement*/, 'scroll').pipe(
-            // pairwise(),
-            //startWith([DEFAULT_SCROLL_POSITION, DEFAULT_SCROLL_POSITION]),
+        fromEvent(window, 'scroll').pipe(
+            // Pass on the needed height values
             map((e: any) => {
-                //console.log(e);
-                //console.log(e.target.scrollingElement.offsetHeight);
-                //console.log(e.target.scrollingElement.clientHeight);
-                //console.log(window.scrollY);
-
-                //console.log(e.target);
-
                 return {
-                    //sH: e.target.scrollHeight,
-                    //sT: e.target.scrollTop,
                     wY: window.scrollY,
                     cH: e.target.body.clientHeight
                 };
             }),
-            //startWith([DEFAULT_SCROLL_POSITION, DEFAULT_SCROLL_POSITION]),
-            // create a stream with the filtered values
-	        // we only need the values from when we are scrolling outside
-	        // our viewport
-	        filter(current => {
-                //console.log(current, window.innerHeight);
-                return current.wY >= current.cH - window.innerHeight
-            }),
-            // we are only interested in the scrollY value of these events
-	        // let's create a stream with only these values
-            //map(() => window.scrollY),
 
-            // Only when the user stops scrolling for 200 ms, we can continue
-	        // so let's debounce this stream for 200 ms
+            // Proceed only when scrolling outside our viewport
+            filter(current => current.wY >= current.cH - window.innerHeight),
+
+            // Only when the user stops scrolling for 100 ms, we can continue
             debounceTime(100),
 
-	        // filter out double values
+            // Filter out double values
             distinct(),
-            //pairwise(),
-            /*
-            tap(data => {
-                prev = data.cH;
-                /*
-                const status = data[1].cH - data[0].cH > 50;
-                console.log(status, data);
-                return status;
-                *
-            })
-            */
-           filter((data) => {
+
+            // Make sure we are scrolling enough distance from previous value.
+            // This makes sure that scrolling events are not triggered in case
+            // only a small scroll advancement was made since last check.
+            filter((data) => {
                 const diff = data.cH - prevHeight;
                 const status = diff > this.threshold;
-                //console.log('fetch', data.cH, prev, diff, status);
                 prevHeight = data.cH;
                 return status;
-           })
+            })
         )
         .subscribe(() => {
-            //if (this.inProgress === false) {
-                //console.log(data);
+            // We are about to fetch some data -> change progress state
+            this.setProcessingState();
 
-                //this.fetch = true;
-                this.setProcessingState();
-                //setTimeout(() => {
-                    //this.callback();
-                    //this.fetch = false;
-                this.callback().subscribe(() => {
-                    this.stateTracker.setSuccess();
-                });
-                //}, 2000);
-            //}
+            // Get the data and changed progress state back
+            this.callback().subscribe(() => {
+                this.stateTracker.setSuccess();
+            });
         });
     }
 
