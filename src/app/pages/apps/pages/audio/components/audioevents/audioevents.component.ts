efc28bbae0dd1ba1b1cc7f6d7bc7de675ef8e8a9
Juha Ojanperä <juha.ojanpera@gmail.com>
2019-02-11 23:03:45 +0200

    Updates.

--- src/app/pages/apps/pages/audio/components/audioevents/audioevents.component.ts
+++ src/app/pages/apps/pages/audio/components/audioevents/audioevents.component.ts
@@ -22,6 +22,49 @@ const MOCKEVENTS: Array<EventModel> = [
 ];
 
 
+class AudioRenderBuffers {
+    private duration = 0;
+    private consume = false;
+    private buffers: Array<any>;
+    private durationThr: number;
+    //private counter = 0;
+
+    constructor(bufferDuration) {
+        this.buffers = [];
+        this.durationThr = bufferDuration;
+    }
+
+    addBuffer(audioBuffer) {
+        /*
+        this.counter++;
+        if (this.counter > 30) {
+            this.counter = 0;
+            this.duration = 0;
+        }
+        */
+
+        this.duration += audioBuffer.duration;
+        this.buffers.push(audioBuffer);
+        this.setConsumeStatus();
+        console.log(this.duration, this.durationThr, this.consume);
+    }
+
+    getBuffer() {
+        if (this.consume && this.buffers.length) {
+            const buffer = this.buffers.shift();
+            this.duration -= buffer.duration;
+            return buffer;
+         }
+
+         return null;
+    }
+
+    private setConsumeStatus() {
+        this.consume = this.duration > this.durationThr;
+    }
+}
+
+
 @Component({
     selector: 'dng-audio-events',
     templateUrl: './audioevents.component.html'
@@ -42,6 +85,10 @@ export class AudioEventsComponent implements OnDestroy {
 
     worker: Worker;
     initTime = undefined;
+
+    allDecoded = false;
+
+    audioBuffers = new AudioRenderBuffers(0.5);
 
     constructor(private api: ApiService) {
         this.audioCtx = new (window['AudioContext'] || window['webkitAudioContext'])();
@@ -69,7 +116,11 @@ export class AudioEventsComponent implements OnDestroy {
         //fetch('/audio-content/adams_xmas_time.mp3')
         fetch('https://fetch-stream-audio.anthum.com/nolimit/house-41000hz-trim.wav')
             .then(response => this.playResponseAsStream(response, 32*1024))
-            .then(_ => console.log('all stream bytes queued for decoding'));
+            .then(() => {
+                this.flush();
+                this.allDecoded = true;
+                console.log('all stream bytes queued for decoding');
+            });
     }
 
     ngOnDestroy() {
@@ -128,9 +179,10 @@ export class AudioEventsComponent implements OnDestroy {
     }
 
     schedulePlayback({channelData, length, numChannels, sampleRate}) {
-        const audioSrc = this.audioCtx.createBufferSource();
+        //const audioSrc = this.audioCtx.createBufferSource();
         const audioBuffer = this.audioCtx.createBuffer(numChannels, length, sampleRate);
 
+        /*
         const onAudioNodeEnded = () => {
             this.audioSrcNodes.shift();
             this.abEnded++;
@@ -143,7 +195,7 @@ export class AudioEventsComponent implements OnDestroy {
             this.el.nativeElement.innerHTML = Math.round(pos);
 
             console.log('ended', this.audioCtx.currentTime,
-                this.abCreated - this.abEnded);
+                this.abCreated - this.abEnded, pos);
         }
 
         audioSrc.onended = onAudioNodeEnded;
@@ -153,6 +205,7 @@ export class AudioEventsComponent implements OnDestroy {
         if (window['webkitAudioContext']) {
             this.audioSrcNodes.push(audioSrc);
         }
+        */
 
       // Use performant copyToChannel() if browser supports it
         for (let c=0; c < numChannels; c++) {
@@ -166,22 +219,96 @@ export class AudioEventsComponent implements OnDestroy {
             }
         }
 
-      // initialize first play position.  initial clipping/choppiness sometimes occurs and intentional start latency needed
-      // read more: https://github.com/WebAudio/web-audio-api/issues/296#issuecomment-257100626
-      if (!this.playStartedAt) {
-        const startDelay = audioBuffer.duration + (this.audioCtx.baseLatency || 128 / this.audioCtx.sampleRate);
-        this.playStartedAt = this.audioCtx.currentTime + startDelay;
-      }
+        this.audioBuffers.addBuffer(audioBuffer);
 
-      audioSrc.buffer = audioBuffer
-      audioSrc.connect(this.audioCtx.destination);
-      audioSrc.start(this.playStartedAt);
-      this.playStartedAt += audioBuffer.duration;
+        const consumeBuffer = this.audioBuffers.getBuffer();
+        if (consumeBuffer) {
+            this.scheduleAudioBuffer(consumeBuffer);
+            /*
+            // initialize first play position.  initial clipping/choppiness sometimes occurs and intentional start latency needed
+            // read more: https://github.com/WebAudio/web-audio-api/issues/296#issuecomment-257100626
+            if (!this.playStartedAt) {
+                const startDelay = consumeBuffer.duration + (this.audioCtx.baseLatency || 128 / this.audioCtx.sampleRate);
+                this.playStartedAt = this.audioCtx.currentTime + startDelay;
+            }
 
-      if (this.initTime === undefined) {
+            audioSrc.buffer = consumeBuffer;
+            audioSrc.connect(this.audioCtx.destination);
+            audioSrc.start(this.playStartedAt);
+            this.playStartedAt += consumeBuffer.duration;
+
+            if (this.initTime === undefined) {
+                this.initTime = this.audioCtx.currentTime;
+                console.log('Init time', this.initTime);
+            }
+            */
+        }
+        /*else {
+            console.log('BUFFER UNDER-RUN');
+            //this.playStartedAt = 0;
+            //this.initTime = undefined;
+        }
+        */
+    }
+
+    scheduleAudioBuffer(audioBuffer) {
+        const audioSrc = this.audioCtx.createBufferSource();
+
+        const onAudioNodeEnded = () => {
+            this.audioSrcNodes.shift();
+            this.abEnded++;
+
+            if (this.abEnded === this.abCreated) {
+                console.log('ALL PROCESSED')
+            }
+
+            const pos = this.audioCtx.currentTime - this.initTime;
+            this.el.nativeElement.innerHTML = Math.round(pos);
+
+            console.log('ended', this.audioCtx.currentTime,
+                this.abCreated - this.abEnded, pos);
+
+            if (!this.allDecoded && this.abCreated - this.abEnded === 0) {
+                this.playStartedAt = 0;
+                console.log('START OVER');
+            }
+        }
+
+        audioSrc.onended = onAudioNodeEnded;
+        this.abCreated++;
+
+        // ensures onended callback is fired in Safari
+        if (window['webkitAudioContext']) {
+            this.audioSrcNodes.push(audioSrc);
+        }
+
+        // initialize first play position.  initial clipping/choppiness sometimes occurs and intentional start latency needed
+            // read more: https://github.com/WebAudio/web-audio-api/issues/296#issuecomment-257100626
+        if (!this.playStartedAt) {
+            const startDelay = audioBuffer.duration + (this.audioCtx.baseLatency || 128 / this.audioCtx.sampleRate);
+            this.initTime = this.audioCtx.currentTime;
+            this.playStartedAt = this.initTime + startDelay;
+        }
+
+        audioSrc.buffer = audioBuffer;
+        audioSrc.connect(this.audioCtx.destination);
+        audioSrc.start(this.playStartedAt);
+        this.playStartedAt += audioBuffer.duration;
+
+        /*
+        if (this.initTime === undefined) {
             this.initTime = this.audioCtx.currentTime;
             console.log('Init time', this.initTime);
-      }
+        }
+        */
+    }
+
+    flush() {
+        let audioBuffer = this.audioBuffers.getBuffer();
+        while (audioBuffer) {
+            this.scheduleAudioBuffer(audioBuffer);
+            audioBuffer = this.audioBuffers.getBuffer();
+        }
     }
 
     togglePlayback() {
