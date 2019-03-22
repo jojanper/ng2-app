53cf0bbfb4f0d83a3a8f9cec81c53dca5ffbc46f
Juha Ojanperä <juha.ojanpera@gmail.com>
2019-03-22 15:36:36 +0200

    Update.

--- src/app/pages/apps/pages/audio/worker/wav.spec.js
+++ src/app/pages/apps/pages/audio/worker/wav.spec.js
@@ -1,5 +1,37 @@
-import { WavDecoder } from './wav';
+import { WavDecoder, PcmDecoder } from './wav';
 
+
+const NSIZE = 6;
+const readBuffer = new ArrayBuffer(NSIZE);
+const writeView = new Uint8Array(readBuffer);
+
+describe('PcmDecoder', () => {
+    beforeAll(() => {
+        writeView[0] = 3;
+        writeView[1] = 2;
+        writeView[2] = 8;
+        writeView[3] = 0;
+        writeView[4] = 1;
+        writeView[5] = 2;
+    });
+
+    it('decode 16-bit audio', () => {
+        const obj = new PcmDecoder();
+        obj.setAudioConfig(48000, 3);
+
+        const result = obj.decode(readBuffer);
+        const audio = result.channelData.map(arrBuffer => new Float32Array(arrBuffer));
+
+        expect(result.length).toEqual(1); // One audio sample available
+        expect(result.numChannels).toEqual(3);
+        expect(result.sampleRate).toEqual(48000);
+        expect(result.channelData.length).toEqual(3);
+
+        expect(audio[0][0]).toEqual((2 * 256 + 3) / 32768);
+        expect(audio[1][0]).toEqual((0 * 256 + 8) / 32768);
+        expect(audio[2][0]).toEqual((2 * 256 + 1) / 32768);
+    });
+});
 
 describe('WavDecoder', () => {
     it('works', () => {
