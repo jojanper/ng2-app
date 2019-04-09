e0a3e548d78a79e66ae072fa55e5e616a8fce8b1
Juha Ojanperä <juha.ojanpera@gmail.com>
2019-04-09 15:19:46 +0300

    Fix.

--- src/app/pages/apps/pages/audio/worker/entry.js
+++ src/app/pages/apps/pages/audio/worker/entry.js
@@ -1,7 +1,7 @@
 import { WavDecoder, PcmDecoder } from './wav';
 
 
-const decoders = {};
+let decoder = null;
 
 const DECODER_TYPES = {
     'audio/pcm-raw': {
@@ -23,17 +23,17 @@ export function eventHandler(event, callback) {
         const { mime } = event.data.config;
         if (Object.prototype.hasOwnProperty.call(DECODER_TYPES, mime)) {
             const config = DECODER_TYPES[event.data.config.mime];
-            decoders[0] = new config.Cls();
+            decoder = new config.Cls();
             if (config.samplerate) {
-                decoders[0].setAudioConfig(config.samplerate, config.channels);
+                decoder.setAudioConfig(config.samplerate, config.channels);
             }
         } else {
             callback({error: `Unsupported mime type ${mime}`});
         }
 
     // Decoding data received -> decode audio
-    } else if (decoders[0] && event.data.decode) {
-        const decoded = decoders[0].decode(event.data.decode);
+    } else if (decoder && event.data.decode) {
+        const decoded = decoder.decode(event.data.decode);
         callback(decoded, decoded.channelData);
     }
 }
